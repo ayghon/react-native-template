0259793dc53df8650d2240d30731b4aa4fccf440
John Doe <johndoe@example.com>
2022-04-02 11:01:13 +0200

    feat(react-navigation): setup the lib, added simple navigation in entrypoint

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen() {
+  const { navigate } = useNavigation();
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button title="Go to Details" onPress={() => navigate('Details')} />
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  const { navigate } = useNavigation();
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button title="Go to Home" onPress={() => navigate('Home')} />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello World !</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
