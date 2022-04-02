fe1c9033c80324969bb3b78a1affe66fac237597
John Doe <johndoe@example.com>
2022-04-02 09:51:06 +0200

    refactor(app): created src folder, cleaned code inside entrypoint

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-/**
- * @format
- */
-
 import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
+import App from './src/app/App';
+import { name as appName } from './src/app/app.json';
 
 AppRegistry.registerComponent(appName, () => App);
