62032f6e67911d40982f60c1deedeca5a6a96947
Aylin <aylin@mirahi.io>
2022-07-01 22:45:03 +0200

    fix(packages): downgraded ora from v6 to v5 to avoid esm compat errors

--- scripts/postInitScript.ts
+++ scripts/postInitScript.ts
@@ -5,11 +5,18 @@ import { inquire } from './generator';
 const spinner = ora('Optional libraries setup');
 
 new Promise((resolve) => {
-  spinner.start();
-  inquire(resolve)
-}).then(() => {
-  spinner.succeed();
-}).catch((error) => {
-  spinner.fail(error);
-  throw new Error('Something went wrong during the post init script execution');
-});
+  // spinner.start();
+    console.log("start !!")
+  inquire(resolve);
+})
+  .then(() => {
+    // spinner.succeed();
+    console.log("hello world !!")
+  })
+  .catch((error) => {
+    // spinner.fail(error);
+    console.log("error !!")
+    throw new Error(
+      'Something went wrong during the post init script execution',
+    );
+  });
